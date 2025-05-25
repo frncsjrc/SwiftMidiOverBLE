f4564561dd3a27f2b3e80ff7c4a897793163ed11
François J. R. CLÉMENT <frncsjrc@free.fr>
2025-05-25 12:41:03 +0200

    Initial Commit

---
+++ SwiftMidiOverBLETests/SwiftMidiOverBLETests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMidiOverBLETests.swift
+//  SwiftMidiOverBLETests
+//
+//  Created by François Jean Raymond CLÉMENT on 25/05/2025.
+//
+
+import Testing
+@testable import SwiftMidiOverBLE
+
+struct SwiftMidiOverBLETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
